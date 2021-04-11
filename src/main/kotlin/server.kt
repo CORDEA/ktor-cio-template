b87e0d66867b775f6c214d14b4d1ef28049170a1
Yoshihiro Tanaka <contact@cordea.jp>
2021-04-11 14:03:46 +0900

    remove unnecessary dependencies

--- src/main/kotlin/server.kt
+++ src/main/kotlin/server.kt
@@ -1,25 +1,4 @@
 import io.ktor.application.*
-import io.ktor.html.respondHtml
-import io.ktor.http.HttpStatusCode
-import io.ktor.routing.get
-import io.ktor.routing.routing
-import kotlinx.html.*
-
-fun HTML.index() {
-    head {
-        title("Hello from Ktor!")
-    }
-    body {
-        div {
-            +"Hello from Ktor"
-        }
-    }
-}
 
 fun Application.main() {
-    routing {
-        get("/") {
-            call.respondHtml(HttpStatusCode.OK, HTML::index)
-        }
-    }
 }
